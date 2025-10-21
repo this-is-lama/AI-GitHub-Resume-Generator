341984d8c09dd78e205357e9dcfd2cef889b2be4
i.ahmatgaliev <islamahmatgaliev0354@gmail.com>
2025-10-21 23:11:50 +0300

    init

---
+++ src/main/java/my/project/aigrg/AiGitHubResumeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package my.project.aigrg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiGitHubResumeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiGitHubResumeGeneratorApplication.class, args);
+	}
+
+}
